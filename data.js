3ebf4689502f2c2a6af241f74ffd17d8ae2ffb3e
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2024-10-30 00:50:54 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,34 @@
+export const firstArray = [
+  {
+    id: 1,
+    arrays: [],
+  },
+  {
+    id: 2,
+    arrays: [],
+  },
+  {
+    id: 3,
+    arrays: [],
+  },
+  {
+    id: 4,
+    arrays: [],
+  },
+  {
+    id: 5,
+    arrays: [],
+  },
+  {
+    id: 6,
+    arrays: [],
+  },
+  {
+    id: 7,
+    arrays: [],
+  },
+  {
+    id: 8,
+    arrays: [],
+  },
+];
---
+++ options.js
@@ -0,0 +1,76 @@
+import { matchingData } from "./main.js";
+import dayjs from "https://unpkg.com/dayjs@1.8.9/esm/index.js";
+/// MAIN STYLE
+export function mainStyle(spanActualMessage) {
+  let style = `
+           <div class="text-container">
+               <div class="user-received-container voice-container">
+                 <p class="voice-message">${spanActualMessage.innerHTML}</p>
+                  <p class="user-received-timer">21:15 PM</p>
+               </div>
+               ${sendMessages()}
+           </div>
+           <div class="input-mic-container">
+             <div class="input-container">
+               <input class="input" type="textArea" placeholder="Message" />
+               <div class="smile-icon">
+                 <ion-icon class='happy-icon' name="happy-outline"></ion-icon>
+               </div>
+               <div class="link-camera">
+                 <ion-icon name="link-outline"></ion-icon>
+                 <ion-icon name="camera-outline"></ion-icon>
+               </div>
+             </div>
+             <div class="mic-container">
+               <ion-icon name="mic-outline"></ion-icon>
+             </div>
+           </div>`;
+  return style;
+}
+
+/// ALTERNATIVE STYLE
+export function newStyles(actualMessage) {
+  if (actualMessage.classList.contains("removed-container")) {
+    let removedMessage = `<div class="text-container">
+           </div>
+           <p class="removed-message">${actualMessage.innerHTML}</p>`;
+    return removedMessage;
+  } else {
+    let voiceMessage = `
+     <div class="text-container">
+         <div class="user-received-container voice-container">
+           <p class="voice-message">${actualMessage.innerHTML}</p>
+            <p class="user-received-timer">21:15 PM</p>
+         </div>;
+         ${sendMessages()}
+     </div>
+      <div class="input-mic-container">
+         <div class="input-container">
+           <input class="input" type="textArea" placeholder="Message" />
+           <div class="smile-icon">
+             <ion-icon class='happy-icon' name="happy-outline"></ion-icon>
+           </div>
+           <div class="link-camera">
+             <ion-icon name="link-outline"></ion-icon>
+             <ion-icon name="camera-outline"></ion-icon>
+           </div>
+         </div>
+         <div class="mic-container">
+           <ion-icon name="mic-outline"></ion-icon>
+         </div>
+       </div>`;
+    return voiceMessage;
+  }
+}
+function sendMessages() {
+  let html = "";
+  const time = dayjs();
+  const timeFormat = time.format("HH:mm A");
+  matchingData.arrays.forEach((userMessage) => {
+    html += `<div class="user-container">
+    <p class="voice-message">${userMessage}</p>
+     <p class="user-timer">${timeFormat}</p>
+ </div>`;
+  });
+  return html;
+}
