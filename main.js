214ce6a4147f093322178bb2d9a8885b63c15fa1
Joel Ohikhena <122086681+Joel1716@users.noreply.github.com>
2024-11-10 01:40:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,8 +4,8 @@ import dayjs from "https://unpkg.com/dayjs@1.8.9/esm/index.js";
 
 /// VARIABLES
 const overallContainer = document.querySelector(".overall-container");
-const containerStyles = overallContainer.innerHTML;
-const mainContainer = document.getElementById("js-messages");
+const messagesContainer = document.getElementById("js-messages");
+const main = document.getElementById("js-main");
 const headerEl = document.getElementById("header");
 
 export let matchingData;
@@ -30,6 +30,7 @@ function containers(event) {
         spanActualMessage === null
           ? newStyles(actualMessage)
           : mainStyle(spanActualMessage);
+      const containerStyles = overallContainer.innerHTML;
       overallContainer.innerHTML = `
       <div class="new-container">
         <header class="new-clicked-header">
@@ -56,7 +57,7 @@ function containers(event) {
       createMessages(actualMessage);
       // STYLE FOR CONTAINER
       headerEl.style.padding = "0";
-      removeMessage();
+      removeMessage(containerStyles);
       break;
     }
     clickedElement = clickedElement.parentElement;
@@ -64,37 +65,43 @@ function containers(event) {
 }
 
 function createMessages(actualMessage) {
+  const sendContainer = document.querySelector(".send-container");
   const inputEl = document.querySelector(".input");
   const textContainer = document.querySelector(".text-container");
   if (actualMessage.classList.contains("removed-container")) {
     return;
   } else {
-    inputEl.addEventListener("keydown", (event) => {
-      if (event.key === "Enter") {
-        const time = dayjs();
-        const timeFormat = time.format("HH:mm A");
-        const newMessageContainer = document.createElement("div");
-        newMessageContainer.classList.add("user-container");
-        newMessageContainer.style.marginBottom = "1rem";
-        const newMessageText = document.createElement("p");
-        newMessageText.innerHTML = inputEl.value;
-        const newMessageTime = document.createElement("p");
-        newMessageTime.classList.add("user-timer");
-        newMessageTime.innerHTML = `${timeFormat} <ion-icon name="checkmark-done-outline"></ion-icon>`;
-        newMessageContainer.append(newMessageText, newMessageTime);
-        textContainer.append(newMessageContainer);
-        sendTextToArray(inputEl, timeFormat);
-        inputEl.value = "";
+      sendContainer.addEventListener("click", () => {
+      if (inputEl.value !== "" && inputEl.value !== "\n") {
+        sendingMessages(inputEl);
       }
     });
   }
+}
+function sendingMessages(inputEl) {
+  const textContainer = document.querySelector(".text-container");
+  /////
+  const time = dayjs();
+  const timeFormat = time.format("HH:mm A");
+  const newMessageContainer = document.createElement("div");
+  newMessageContainer.classList.add("user-container");
+  newMessageContainer.style.marginBottom = "1rem";
+  const newMessageText = document.createElement("p");
+  newMessageText.innerHTML = inputEl.value;
+  const newMessageTime = document.createElement("p");
+  newMessageTime.classList.add("user-timer");
+  newMessageTime.innerHTML = `${timeFormat} <ion-icon name="checkmark-done-outline"></ion-icon>`;
+  newMessageContainer.append(newMessageText, newMessageTime);
+  textContainer.append(newMessageContainer);
+  sendTextToArray(inputEl, timeFormat);
+  inputEl.value = "";
 }
 
 function removeMessage() {
   document.querySelector(".arrow-back").addEventListener("click", () => {
     overallContainer.innerHTML = containerStyles;
-    const mainContainer = document.getElementById("js-messages");
-    mainContainer.addEventListener("click", containers);
+    const messagesContainer = document.getElementById("js-messages");
+    messagesContainer.addEventListener("click", containers);
   });
 }
 
